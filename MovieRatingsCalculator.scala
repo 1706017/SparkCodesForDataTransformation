79b60fdb5baaba9f499981e8c6359a6ab8f2789c
AMRIT MANASH <58604913+1706017@users.noreply.github.com>
2023-01-02 13:30:35 +0530

    Update MovieRatingsCalculator.scala

--- MovieRatingsCalculator.scala
+++ MovieRatingsCalculator.scala
@@ -17,3 +17,30 @@ object MovieRatingsCalculator extends App
   
   sortedRdd.collect.foreach(println)
 }
+
+//Equivalent Pyspark code
+
+/*
+
+from pyspark import SparkContext
+
+sc = SparkContext("local[*]","wordcount")
+
+base_Rdd = sc.textFile("file:///C:/Users/KIIT/Desktop/DATA/movieRatingsData.txt")
+
+extracted_Rdd = base_Rdd.map(lambda x : x.split("\t")[2])
+
+mapped_Rdd = extracted_Rdd.map(lambda x : (x,1))
+
+final_rdd = mapped_Rdd.reduceByKey( lambda x,y :(x+y))
+
+result = final_rdd.sortByKey().False
+
+final_result = result.collect()
+
+for i in final_result:
+    print(i)
+
+
+
+*/
